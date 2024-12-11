8a460e63b9fd97e67a3376ce492db91add52bf8a
Casmir Onyeka <casmir293@gmail.com>
2024-12-11 16:33:30 +0100

    first commit.

---
+++ src/components/Child.vue
@@ -0,0 +1,7 @@
+<template>
+  <button @click="emit('greetings')">Click Me!</button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["greetings"]);
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Child from "@/components/Child.vue";
+import { ref } from "vue";
+
+const msg = ref("Hello World!");
+
+const updateGreetings = () => {
+  msg.value = "I'm learning Vitest!";
+};
+</script>
+
+<template>
+  <main>
+    <h1>{{ msg }}</h1>
+    <Child @greetings="updateGreetings" />
+  </main>
+</template>
